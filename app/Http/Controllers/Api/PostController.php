5dda7717a6d3a75edec1d6816c83797af5df1ebb
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-03-22 12:36:30 +0545

    return type declaration in post controller functions

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,33 +4,34 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         Post::create($request->all());
         return response()->json('Post Created successfully.', 201);
     }
 
-    public function show(Post $post)
+    public function show(Post $post): JsonResponse
     {
         return response()->json($post, 200);
     }
 
-    public function update(Request $request, Post $post)
+    public function update(Request $request, Post $post): JsonResponse
     {
         $post->update($request->all());
         return response()->json($post, 200);
     }
 
-    public function index()
+    public function index(): JsonResponse
     {
         return response()->json(Post::all(), 200);
     }
 
-    public function destroy(Post $post)
+    public function destroy(Post $post): JsonResponse
     {
         $post->delete();
         return response()->json('Deleted', 204);
